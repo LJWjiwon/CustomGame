105e138a1d9d147bb5531adf2776002228ab06e6
이지원 <ljiwon77733@gmail.com>
2024-08-30 15:21:13 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpForce = 100f;
+    private Rigidbody2D PlayerRigidbody;
+    public bool isGround;
+
+    public void Update()
+    {
+        PlayerRigidbody = GetComponent<Rigidbody2D>();
+
+        //if (!GameManager.instance.GameOver)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if(isGround == true)
+                {
+                    PlayerRigidbody.velocity = Vector2.zero;
+                    PlayerRigidbody.AddForce(new Vector2(0, jumpForce));
+
+                    isGround = false;
+
+                    if (Input.GetKeyUp(KeyCode.Space))
+                    {
+                        PlayerRigidbody.velocity = PlayerRigidbody.velocity * 0.7f;
+                    }
+                }
+            }
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGround = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class In : MonoBehaviour
+{
+    public GameObject[] Item;
+    public GameObject[] SpawnLocation;
+    private float lastSpawnTime;
+    private float timeBetSpawn;
+    private float xPos;
+    private float yPos;
+
+    private void Start()
+    {
+        lastSpawnTime = 0f;
+        timeBetSpawn = 1f;
+        yPos = SpawnLocation[0].transform.position.y;
+    }
+
+    private void Update()
+    {
+        xPos = Random.Range(SpawnLocation[0].transform.position.x, SpawnLocation[1].transform.position.x);
+
+
+        if(Time.time >= lastSpawnTime + timeBetSpawn)
+        {
+            if (Random.Range(0, 5) == 0)
+            {
+                Item[0].transform.position = new Vector3(xPos, yPos);
+                Instantiate(Item[0], Item[0].transform.position, Item[0].transform.rotation);
+            }
+            else
+            {
+                Item[1].transform.position = new Vector3(xPos, yPos);
+                Instantiate(Item[1], Item[1].transform.position, Item[1].transform.rotation);
+            }
+            lastSpawnTime = Time.time;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+}
